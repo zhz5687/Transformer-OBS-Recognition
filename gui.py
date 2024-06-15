e92b8f52d06f798b76c1ed45a7a87cfc040fc75b
zhz5687 <zhz5687@qq.com>
2024-06-15 20:42:21 +0800

    add resnet model

--- gui.py
+++ gui.py
@@ -18,49 +18,7 @@ from PIL import Image
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 import json
-
-class Residual(nn.Module):
-    def __init__(self, input_channels, min_channels, num_channels,
-                 use_1x1conv=False, strides=1):
-        super().__init__()
-        self.conv1 = nn.Conv2d(input_channels, min_channels,
-                               kernel_size=1)
-        self.conv2 = nn.Conv2d(min_channels, min_channels,
-                               kernel_size=3, padding=1, stride=strides)
-        self.conv3 = nn.Conv2d(min_channels, num_channels,
-                               kernel_size=1)
-        if use_1x1conv:
-            self.conv4 = nn.Conv2d(input_channels, num_channels,
-                                   kernel_size=1, stride=strides)
-        else:
-            self.conv4 = None
-        self.bn1 = nn.BatchNorm2d(min_channels)
-        self.bn2 = nn.BatchNorm2d(min_channels)
-        self.bn3 = nn.BatchNorm2d(num_channels)
-
-    def forward(self, X):
-        Y = F.relu(self.bn1(self.conv1(X)))
-        Y = self.bn2(self.conv2(Y))
-        Y = self.bn3(self.conv3(Y))
-        if self.conv4:
-            X = self.conv4(X)
-        Y += X
-        return F.relu(Y)
-
-
-
-def resnet_block(input_channels, min_channels, num_channels, num_residuals, stride,
-                 first_block=False):
-    blk = []
-    for i in range(num_residuals):
-        if i == 0 and not first_block:
-            blk.append(Residual(input_channels, min_channels, num_channels,
-                                use_1x1conv=True, strides=stride))
-        elif first_block and i == 0:
-            blk.append(Residual(input_channels, min_channels, num_channels, use_1x1conv=True))
-        else:
-            blk.append(Residual(num_channels, min_channels, num_channels))
-    return blk
+from src.model import ResNet
 
 class TestData(Dataset):
     def __init__(self, transform=None):
@@ -234,18 +192,7 @@ class MyForm(QMainWindow, Ui_MainWindow):
         self.ui.label_english.setText("English:")
         self.ui.label_chinese.setText("Chinese 中文: ")
 
-        b1 = nn.Sequential(nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
-                   nn.BatchNorm2d(64), nn.ReLU(),
-                   nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
-
-        b2 = nn.Sequential(*resnet_block(64, 64, 256, 3, 2, first_block=True))
-        b3 = nn.Sequential(*resnet_block(256, 128, 512, 4, 2))
-        b4 = nn.Sequential(*resnet_block(512, 256, 1024, 6, 2))
-        b5 = nn.Sequential(*resnet_block(1024, 512, 2048, 2, 2))
-        self.model = nn.Sequential(b1, b2, b3, b4, b5,
-                            nn.AdaptiveAvgPool2d((1, 1)),
-                            nn.Flatten(), nn.Linear(2048, 1588))
-
+        self.model = ResNet()
         checkpoint = torch.load('./checkpoint_ep0600.pth',  map_location=torch.device('cpu'))
         self.model.load_state_dict(checkpoint['state_dict'])
         self.model.eval()
--- train.py
+++ train.py
@@ -15,7 +15,7 @@ import math
 import argparse
 from tqdm import tqdm
 import pandas as pd
-
+from src.model import ResNet
 
 
 """### Set arguments"""
@@ -218,65 +218,8 @@ train_loader = DataLoader(train_dataset, shuffle=True, batch_size = args.batch_s
 test_dataset = TestData()
 test_loader = DataLoader(test_dataset, shuffle=True, batch_size = args.batch_size, num_workers=args.num_workers, pin_memory=True)
 
-
-class Residual(nn.Module):
-    def __init__(self, input_channels, min_channels, num_channels,
-                 use_1x1conv=False, strides=1):
-        super().__init__()
-        self.conv1 = nn.Conv2d(input_channels, min_channels,
-                               kernel_size=1)
-        self.conv2 = nn.Conv2d(min_channels, min_channels,
-                               kernel_size=3, padding=1, stride=strides)
-        self.conv3 = nn.Conv2d(min_channels, num_channels,
-                               kernel_size=1)
-        if use_1x1conv:
-            self.conv4 = nn.Conv2d(input_channels, num_channels,
-                                   kernel_size=1, stride=strides)
-        else:
-            self.conv4 = None
-        self.bn1 = nn.BatchNorm2d(min_channels)
-        self.bn2 = nn.BatchNorm2d(min_channels)
-        self.bn3 = nn.BatchNorm2d(num_channels)
-
-    def forward(self, X):
-        Y = F.relu(self.bn1(self.conv1(X)))
-        Y = self.bn2(self.conv2(Y))
-        Y = self.bn3(self.conv3(Y))
-        if self.conv4:
-            X = self.conv4(X)
-        Y += X
-        return F.relu(Y)
-
-
-b1 = nn.Sequential(nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
-                   nn.BatchNorm2d(64), nn.ReLU(),
-                   nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
-
-
-def resnet_block(input_channels, min_channels, num_channels, num_residuals, stride,
-                 first_block=False):
-    blk = []
-    for i in range(num_residuals):
-        if i == 0 and not first_block:
-            blk.append(Residual(input_channels, min_channels, num_channels,
-                                use_1x1conv=True, strides=stride))
-        elif first_block and i == 0:
-            blk.append(Residual(input_channels, min_channels, num_channels, use_1x1conv=True))
-        else:
-            blk.append(Residual(num_channels, min_channels, num_channels))
-    return blk
-
-
-b2 = nn.Sequential(*resnet_block(64, 64, 256, 3, 2, first_block=True))
-b3 = nn.Sequential(*resnet_block(256, 128, 512, 4, 2))
-b4 = nn.Sequential(*resnet_block(512, 256, 1024, 6, 2))
-b5 = nn.Sequential(*resnet_block(1024, 512, 2048, 2, 2))
-net = nn.Sequential(b1, b2, b3, b4, b5,
-                    nn.AdaptiveAvgPool2d((1, 1)),
-                    nn.Flatten(), nn.Linear(2048, 1588))
-
+net = ResNet()
 net = net.cuda(0)
-
 
 def init_weights(m):
     if type(m) == nn.Linear or type(m) == nn.Conv2d:
